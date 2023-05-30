30bc764d92bf9b76a74a6312573589187ff2f604
Jaes98 <marcus.jaes@gmail.com>
2023-05-30 11:42:24 +0200

    øvelse done, ikke sikker på om sort virker tho, alfabetisk

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+let students = [];
+
+function initApp(params) {
+    document.querySelector("#create-student-form").addEventListener("submit", addStudent);
+}  
+
+function addStudent(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const name = form.name.value   ;
+    const email = form.email.value;
+    const age = Number(form.age.value);
+    const newStudent = ({name, email, age})
+
+    students.push(newStudent)
+    console.log("student", students);
+    newStudents(students)
+}
+
+function newStudents(params) {
+    document.querySelector("tbody").innerHTML = "";
+    students.sort((a,b) => a.name.localeCompare(b.name));
+   for (const student of students) {
+    if (student.age >= 18) {
+    showStudents(student);
+    }
+   }
+}
+
+function showStudents(student) {
+    const myHTML = 
+    `
+    <tr>
+    <td>${student.name}</td>
+    <td>${student.email}</td>
+    <td>${student.age}</td>
+    </tr>
+    `
+    document.querySelector("tbody").insertAdjacentHTML("beforeend", myHTML);
+}
